d587c0ba2a3524454e71e65a3fbbc5273961f320
Vlad <blackboxorange@gmail.com>
2024-09-13 02:54:01 +0400

    hw3_complite

---
+++ Basic_C/hw3/hw3_task5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, result = 1;
+    scanf("%d", &a);
+
+    for (int i = 0; i < 3; i++)
+    {
+        result *= a % 10;
+        a /= 10;   
+    }
+
+    printf("%d\n", result); 
+    return 0;
+}
